dbcca4be97e7eb3437b060a8f00e295b44931df6
Jerry Tapia <jerryxtapia@gmail.com>
2022-08-08 12:16:13 -0700

    familiarizing myself with PUG

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // console.log('hey');
+  // res.send('Hey! It works!');
+  const jerry = { name: 'Jerry', age: 25};
+  res.render('hello', {
+    name: 'Jerry',
+    dog: req.query.dog,
+  });
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+})
 
 module.exports = router;
