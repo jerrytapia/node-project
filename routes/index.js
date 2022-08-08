082902ddd79367d52c592ae906f8fc7838f7a93f
Jerry Tapia <jerryxtapia@gmail.com>
2022-08-08 12:57:42 -0700

    very very initial routes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
-// Do work here
-router.get('/', (req, res) => {
-  // console.log('hey');
-  // res.send('Hey! It works!');
-  const jerry = { name: 'Jerry', age: 25};
-  res.render('hello', {
-    name: 'Jerry',
-    dog: req.query.dog,
-  });
-});
+router.get('/', storeController.homePage)
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-})
 
 module.exports = router;
